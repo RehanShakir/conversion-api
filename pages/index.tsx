47676921aa549c84b618ec18905de461450ee579
RehanShakir <rehanshakir2@gmail.com>
2023-02-27 16:03:25 +0500

    snap pixel

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,27 +33,19 @@ export default function Home() {
 
         <script>
           {`
-             !function(f,b,e,v,n,t,s)
-             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-             if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-             n.queue=[];t=b.createElement(e);t.async=!0;
-             t.src=v;s=b.getElementsByTagName(e)[0];
-             s.parentNode.insertBefore(t,s)}(window, document,'script',
-             'https://connect.facebook.net/en_US/fbevents.js');
-             fbq('init', '707781711039772');
-             fbq('track', 'PageView');
+            (function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+            {a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+            a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+            r.src=n;var u=t.getElementsByTagName(s)[0];
+            u.parentNode.insertBefore(r,u);})(window,document,
+            'https://sc-static.net/scevent.min.js');
+             snaptr('init', '4992eea0-c5da-4cf7-9950-ad5fdc5f634e', {
+            'user_email': '__INSERT_USER_EMAIL__'
+            });
+            snaptr('track', 'PAGE_VIEW');
+            snaptr('track', 'SUBSCRIBE');
 `}
         </script>
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=707781711039772&ev=PageView&noscript=1"
-            alt="fb"
-          />
-        </noscript>
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
